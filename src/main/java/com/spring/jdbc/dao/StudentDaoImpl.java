40f019112b04c5f463f643f3e02e2ce199e05a62
Nityanand Singh <nns.nitya@gmail.com>
2024-01-24 23:13:29 +0530

    RowMapper as anonymous class
    but it is not recommended for multiple use

--- src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -1,5 +1,8 @@
 package com.spring.jdbc.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -32,8 +35,16 @@ public class StudentDaoImpl implements StudentDao {
 	public Student getStudent(int studentId) {
 		// Selecting single student data
 		String query = "select * from student where id=?";
-		RowMapper<Student> rowMapper = new RowMapperImpl();
-		Student student = this.jdbcTemplate.queryForObject(query, rowMapper, studentId);
+		//RowMapper<Student> rowMapper = new RowMapperImpl();
+		Student student = this.jdbcTemplate.queryForObject(query, new RowMapper() {
+			public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Student student = new Student();
+				student.setId(rs.getInt(1));
+				student.setName(rs.getString(2));
+				student.setCity(rs.getString(3));
+				return student;
+			}
+		}, studentId);
 		return student;
 	}
 	
