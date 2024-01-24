89c6277d15749ab7a24378ba9f853ee1695e7408
Nityanand Singh <nns.nitya@gmail.com>
2024-01-24 22:35:19 +0530

    insert operation performed
    with xml config

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -2,7 +2,8 @@ package com.spring.jdbc;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
+import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.entities.Student;
 
 /**
  * Hello world!
@@ -15,13 +16,21 @@ public class App
         System.out.println("My program started...");
         //Spring jdbc=>JdbcTemplate
         ApplicationContext context=new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
-        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
-
+        /*JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
         //insert query
         String query="insert into student(id, name, city) values(?,?,?)";
         
         //fire the query
-        int result = template.update(query, 457, "Nityanand", "Gorakhpur");
-        System.out.println("number of record inserted.."+result);
+        int result = template.update(query, 457, "Nityanand", "Gorakhpur");*/
+
+        StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+        Student student =new Student();
+        student.setId(630);
+        student.setName("Pankaj Kr Singh");
+        student.setCity("Delhi");
+        
+        int result= studentDao.insert(student);
+        System.out.println("student added: "+result);
+
     }
 }
